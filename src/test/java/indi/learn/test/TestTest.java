aa2b2a69332bffa69a46310a61dd1e3ca78f4532
YinKailin <YinKailin>
2017-04-24 12:35:19 +0800

    add a test file

---
+++ src/test/java/indi/learn/test/TestTest.java
@@ -0,0 +1,13 @@
+package indi.learn.test;
+
+import java.sql.Timestamp;
+
+public class TestTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Timestamp begin = new Timestamp(1493118121000L);
+		System.out.println(begin);
+	}
+
+}
